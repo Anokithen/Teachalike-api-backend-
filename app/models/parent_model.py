535c9e477e4e0ab80ce5acff952f1068b417237d
Anokithen <anokithen@gmail.com>
2026-07-29 12:48:49 +0530

    feat(models): complete parent model

--- app/models/parent_model.py
+++ app/models/parent_model.py
@@ -37,3 +37,29 @@ class Parent(db.Model):
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == ROLE_ADMIN
+
+    @property
+    def is_teacher(self):
+        return self.role == ROLE_TEACHER
+
+    @property
+    def is_parent(self):
+        return self.role == ROLE_PARENT
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "role": self.role,
+            "is_banned": self.is_banned,
+            "profile_image_url": self.profile_image_url,
+            "created_at": utc_isoformat(self.created_at),
+        }
