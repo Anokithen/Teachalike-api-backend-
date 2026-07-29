4dd550fa2f86deaa5cc284363a6bda95fa73b62a
Anokithen <anokithen@gmail.com>
2026-07-29 12:48:50 +0530

    feat(child): establish module foundation

---
+++ app/routes/child_routes.py
@@ -0,0 +1,39 @@
+from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from app.controllers import child_controller as ctrl
+from app.controllers import reading_session_controller as session_ctrl
+from app.controllers import game_result_controller as result_ctrl
+from app.controllers import leaderboard_controller as leaderboard_ctrl
+from app.middleware import parent_or_teacher_required
+
+child_bp = Blueprint("child", __name__, url_prefix="/api/children")
+
+
+@child_bp.route("", methods=["POST"])
+@parent_or_teacher_required
+def create_child():
+    return ctrl.create_child()
+
+
+@child_bp.route("", methods=["GET"])
+@jwt_required()
+def list_children():
+    return ctrl.list_children()
+
+
+@child_bp.route("/<int:child_id>", methods=["GET"])
+@jwt_required()
+def get_child(child_id):
+    return ctrl.get_child(child_id)
+
+
+@child_bp.route("/<int:child_id>", methods=["PATCH"])
+@jwt_required()
+def update_child(child_id):
+    return ctrl.update_child(child_id)
+
+
+@child_bp.route("/<int:child_id>/verify-pin", methods=["POST"])
+@jwt_required()
+def verify_child_pin(child_id):
+    return ctrl.verify_child_pin(child_id)
